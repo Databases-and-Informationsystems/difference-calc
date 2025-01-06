4b863f31f4c134f6e1a9101fbc089f0d846ffa56
luisrab <luis.rabenstein.lr@gmail.com>
2025-01-06 16:27:14 +0000

    Revert "feat: score calculation"
    This reverts commit 3d623a4056341624149ffcc1f1bac480bfae235e.

--- app/model/similarity_score.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-class SimilarityScoreResponse(BaseModel):
-    mention_score: float
-    # considered_entitiy_quote: float
-    # entity_score: float
-    considered_relation_quote: float
-    relation_score: float
